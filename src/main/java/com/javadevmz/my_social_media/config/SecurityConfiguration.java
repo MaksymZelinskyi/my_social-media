d1d8964a2e50bdf464e101a7edbe80202460b763
Maksym Zelinskyi <maximzelinskyi@gmail>
2024-09-15 19:39:33 +0200

    Disable CSRF protection

--- src/main/java/com/javadevmz/my_social_media/config/SecurityConfiguration.java
+++ src/main/java/com/javadevmz/my_social_media/config/SecurityConfiguration.java
@@ -1,15 +1,23 @@
 package com.javadevmz.my_social_media.config;
 
-import com.javadevmz.my_social_media.service.InDBUserDetailsManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -19,19 +27,40 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
        return http.authorizeHttpRequests(auth -> {
             auth.requestMatchers("/login").permitAll()
-                    .anyRequest().authenticated();
+                    .requestMatchers("/sign-up").permitAll()
+                    .anyRequest().permitAll();
        })
+               .csrf(new Customizer<CsrfConfigurer<HttpSecurity>>() {
+                   @Override
+                   public void customize(CsrfConfigurer<HttpSecurity> csrfConfigurer) {
+                       csrfConfigurer.disable();
+                   }
+               })
                .httpBasic(Customizer.withDefaults())
                .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new InDBUserDetailsManager();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CorsFilter corsConfig(){
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+    config.setAllowedHeaders(Arrays.asList(
+            HttpHeaders.ORIGIN,
+            HttpHeaders.CONTENT_TYPE,
+            HttpHeaders.ACCEPT,
+            HttpHeaders.AUTHORIZATION
+            )
+    );
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+    }
+
 }
