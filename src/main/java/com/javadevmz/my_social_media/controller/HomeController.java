c8da27502ec576562c3b05a7282f3f945f3bbe1d
Maksym Zelinskyi <maximzelinskyi@gmail>
2024-09-22 18:47:55 +0200

    Change the login method's mapping from 'Get' to 'Post'

--- src/main/java/com/javadevmz/my_social_media/controller/HomeController.java
+++ src/main/java/com/javadevmz/my_social_media/controller/HomeController.java
@@ -37,7 +37,7 @@ public class HomeController {
         return posts;
     }
 
-    @GetMapping("/login")
+    @PostMapping("/login")
     public void login(AuthenticationRequest authenticationRequest){
 
     }
